625f472598935f3d29f3532f13d4a13871c26b37
rafaelcminovare-netizen <rafaelcm.inovare@gmail.com>
2026-05-12 11:01:11 -0300

    add - style

---
+++ exercicio8.js
@@ -0,0 +1,9 @@
+let a = 10;
+let b = 5;
+
+let diferenca = a - b;
+let somaDobroTriplo = (2 * a) + (3 * b);
+let multiplicacao = a * b;
+
+document.getElementById("resultado8").innerHTML = `Diferença: ${diferenca}, Dobro da 1ª e triplo da 2ª: ${somaDobroTriplo}, Multiplicação: ${multiplicacao}`;
+document.getElementById("resultado8").innerHTML += "<br>Operações com Duas Variáveis";
